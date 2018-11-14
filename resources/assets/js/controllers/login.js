f7cdb3e9c77c17b55473ab7c9bfeb95a87864039
Felipe Andrade <felipe.andrade@jsl.com.br>
2018-11-14 16:38:37 -0200

    authentication with angular-oauth2 not completed (v1)

--- resources/assets/js/controllers/login.js
+++ resources/assets/js/controllers/login.js
@@ -1,22 +1,30 @@
 angular.module('app.controllers')
-        .controller('LoginController', ['$scope','$location', 'OAuth', function($scope, $location,OAuth){
+        .controller('LoginController', ['$scope', '$location', 'OAuth', function ($scope, $location, OAuth) {
                 $scope.user = {
                     username: '',
                     password: ''
                 }
-                
-                $scope.login = function(){
-                    
+
+                $scope.login = function () {
+
                     //console.log($scope.user);
+
+                    var success = function () {
+                        //$location.path('home');
+                        alert('success');
+                    };
+
+                    var fail = function () {
+                        //alert('login invalido');
+                        alert('fail');
+                    };
+
+                    // https://github.com/oauthjs/angular-oauth2
+                    // https://github.com/oauthjs/angular-oauth2/blob/master/dist/angular-oauth2.js 
+
+                    OAuth.getAccessToken($scope.user).then(success, fail);
                     
-                    OAuth.getAccessToken($scope.user).then(
-                            function(){ //sucess
-                                $location.path('home');
-                            },
-                            function(){ //error
-                                alert('login invalido');
-                            });
                 }
-        }]);
-    
+            }]);
+
     
